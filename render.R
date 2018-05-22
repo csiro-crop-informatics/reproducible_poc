ad3096c905bb0f532f5328ab004e6eb8456c2912
Radoslaw Suchecki <r.suchecki@gmail.com>
2018-05-22 14:36:50 +0930

    rendering in place, time for some content

--- render.R
+++ render.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#required modules if executed on the cluster: R/3.4.4 pandoc/1.12.3 
+#required modules if executed on the cluster: R/3.4.4 pandoc/1.12.3
 
 if(!require(rmarkdown)){
     install.packages("rmarkdown")
@@ -9,11 +9,9 @@ if(!require(rmarkdown)){
 
 if(!require(revealjs)){
     location <- "~/local/R_libs/"
-    dir.create(location, recursive = TRUE) 
+    dir.create(location, recursive = TRUE)
     install.packages("revealjs", lib=location, repos='https://cran.csiro.au')
     library(revealjs, lib.loc=location)
 }
 
-rmarkdown::render("doc.Rmd", output_format = "all")
 rmarkdown::render("show.Rmd", output_format = "all")
-
