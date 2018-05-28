df084a298554d947df0294a50a21f34902745fae
Radoslaw Suchecki <r.suchecki@gmail.com>
2018-05-28 16:17:39 +0930

    re-formatting

--- bin/extractStatsFromBAM.sh
+++ bin/extractStatsFromBAM.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 awk -vOFS="\t" '{split($1,sim,"|");if(sim[4]==$3 && sim[5]==$4-1){count++}};END{print count,NR,count/NR}' ${@:-/dev/stdin}
 
 
--- bin/fasta2fastqDummy.sh
+++ bin/fasta2fastqDummy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 sed 's/^>/@/'  ${@:-/dev/stdin} | paste - - | awk -vFS="\t" -vOFS="\n" '{print $1,$2,"+";gsub(".","a",$2);print $2}'
 
